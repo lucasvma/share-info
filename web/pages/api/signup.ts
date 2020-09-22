897c1fb30d734ed89fe4d1bdf6cabe5c04b586a9
lucasvma <venturaml21@gmail.com>
2020-09-22 00:00:43 -0300

    Feat: Signup/Login Windows

---
+++ web/pages/api/signup.ts
@@ -0,0 +1,42 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import { MongoClient, Db } from 'mongodb';
+import url from 'url';
+
+let cachedDb: Db = null;
+
+async function connectToDatabase(uri: string) {
+    if (cachedDb) {
+        return cachedDb;
+    }
+
+    const client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const dbName = url.parse(uri).pathname.substr(1);
+
+    const db = client.db(dbName);
+
+    cachedDb = db;
+
+    return db;
+}
+
+export default async (request: NowRequest, response: NowResponse) => {
+    const { name, email, password, bio } = request.body;
+
+    const db = await connectToDatabase(process.env.MONGODB_URI);
+
+    const collection = db.collection('users');
+
+    await collection.insertOne({
+        name,
+        email,
+        password,
+        bio,
+        createdAt: new Date(),
+    });
+
+    return response.status(201).json({ message: 'Your account has been created successfully' });
+}
---
+++ web/pages/signup.tsx
@@ -0,0 +1,98 @@
+import { useState, FormEvent } from 'react';
+import { Flex, Button, Text, Link } from '@chakra-ui/core';
+import axios from 'axios';
+import Input from '../components/Input';
+
+export default function SingUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [bio, setBio] = useState('');
+
+  function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+
+    axios.post('/api/signup', { name, email, password, bio });
+  }
+
+  return (
+      <Flex
+        as="main"
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Flex
+          as="form"
+          onSubmit={handleSignUp}
+          backgroundColor="gray.700"
+          borderRadius="md"
+          flexDir="column"
+          alignItems="stretch"
+          padding={8}
+          marginTop={4}
+          width="100%" 
+          maxW="400px"
+        >
+          <Text
+            textAlign="center"
+            fontSize="lg"
+            fontWeight="bold"
+            color="gray.400"
+          >
+            Crie sua conta
+          </Text>
+
+          <Input
+            placeholder="Nome"
+            value={name}
+            marginTop={6}
+            onChange={e => setName(e.target.value)}
+          />
+
+          <Input
+            placeholder="E-mail"
+            type="email"
+            value={email}
+            marginTop={2}
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <Input 
+            placeholder="Senha"
+            type="password"
+            value={password}
+            marginTop={2}
+            onChange={e => setPassword(e.target.value)}
+          />
+        
+          <Input
+            placeholder="Bio"
+            value={bio}
+            onChange={e => setBio(e.target.value)}
+            marginTop={2}
+          />
+
+          <Button
+            type="submit"
+            backgroundColor="purple.500"
+            height="50px"
+            borderRadius="sm"
+            marginTop={6}
+            _hover={{ backgroundColor: 'purple.600' }}
+          >
+            INSCREVER
+          </Button>
+
+          <Link
+            marginTop={2}
+            href='/'
+            fontSize="sm"
+            textAlign="right"
+          >
+            <a>Voltar</a>
+          </Link>
+        </Flex>
+      </Flex>
+    );
+}
